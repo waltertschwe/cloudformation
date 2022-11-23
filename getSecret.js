d2dfd299a9329e15a47e86473140a129f5766ff0
Walter Schweitzer <wschweitzer00@gmail.com>
2022-11-23 01:29:01 -0500

    adding header data

--- getSecret.js
+++ getSecret.js
@@ -2,12 +2,14 @@ const AWS = require('aws-sdk')
 
 exports.lambda_handler = async (event, context) => {
     console.log("event = ", event);
-    let requestData = {};
     
     const secretName = JSON.parse(event.body).secretName;
     if(typeof secretName === 'undefined') {
         return {
             statusCode:400,
+            headers: {
+                'Content-Type': 'application/json',
+            },
             body: JSON.stringify({"error": "Secret Service require a 'secretName' in the body request"})
         }
     }
@@ -17,12 +19,18 @@ exports.lambda_handler = async (event, context) => {
         const secretData = await secretManager.getSecretValue({ SecretId: secretName }).promise();
         return {
             statusCode: 200,
-            body: JSON.stringify(secretData.SecretString),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: secretData.SecretString,
         };
     } catch(e) {
         
         return {
             statusCode: 400,
+            headers: {
+                'Content-Type': 'application/json',
+            },
             body: JSON.stringify({"error": "Could not find secret value for secret name:" + secretName})
         }
     }
