e0a45b2347f1bec590913c3bf90c709c0c59e331
Walter Schweitzer <wschweitzer00@gmail.com>
2022-11-23 01:18:23 -0500

    updating lambda

--- getSecret.js
+++ getSecret.js
@@ -1,22 +1,29 @@
-// const SecretsManager = require('./AwsSecretsManager.js');
 const AWS = require('aws-sdk')
 
 exports.lambda_handler = async (event, context) => {
     console.log("event = ", event);
     let requestData = {};
+    
     const secretName = JSON.parse(event.body).secretName;
-        
-    console.log("request secret = " + secretName);
-    var region = 'us-east-1';
+    if(typeof secretName === 'undefined') {
+        return {
+            statusCode:400,
+            body: JSON.stringify({"error": "Secret Service require a 'secretName' in the body request"})
+        }
+    }
 
-    
     let secretManager = new AWS.SecretsManager({ region: 'us-east-1' });
-    const secretData = await secretManager.getSecretValue({ SecretId: secretName }).promise();
-    console.log(`data is: ${JSON.stringify(secretData.SecretString)}`);
-    
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(secretData.SecretString),
-    };
-    return response;
+    try {
+        const secretData = await secretManager.getSecretValue({ SecretId: secretName }).promise();
+        return {
+            statusCode: 200,
+            body: JSON.stringify(secretData.SecretString),
+        };
+    } catch(e) {
+        
+        return {
+            statusCode: 400,
+            body: JSON.stringify({"error": "Could not find secret value for secret name:" + secretName})
+        }
+    }
 };
