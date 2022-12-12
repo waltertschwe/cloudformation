e15df69641ef54b09bc0d52d45335271ecc3236d
Walter Schweitzer <wschweitzer00@gmail.com>
2022-12-11 21:24:28 -0500

    80% coverage

--- tests/unit/test-handler.test.ts
+++ tests/unit/test-handler.test.ts
@@ -14,3 +14,17 @@ describe('Test a valid request where a secret is not found.', function () {
         expect(result.body).toStrictEqual(`{"error":"Could not find secret value for secret name: ${event.body.secretName}"}`);
     });
 });
+
+describe('Test a invalid request where a secretName key is not provided.', function () {
+    it('verifies a secret was not found.', async () => {
+        const event: APIGatewayProxyEvent = {
+            body: {
+                secretName23: "invalid-secret-key"
+            }
+        } as any
+        const result = await handler(event)
+
+        expect(result.statusCode).toEqual(400);
+        expect(result.body).toStrictEqual(`{"error":"Secret Service require a 'secretName' in the body request"}`);
+    });
+});
